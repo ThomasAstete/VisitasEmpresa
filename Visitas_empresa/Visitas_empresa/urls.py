925713ca929ce3c31f64e6effeac0d13cd242137
Thomas Astete <thomas.astete@inacapmail.cl>
2025-09-29 14:06:36 -0300

    correcciones menores

--- Visitas_empresa/Visitas_empresa/urls.py
+++ Visitas_empresa/Visitas_empresa/urls.py
@@ -14,15 +14,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    # PANEL DE ADMINISTRACIÓN DE DJANGO
-    # URL: http://dominio.com/admin/
-    # Proporciona interfaz web para gestionar modelos y datos
-    # Requiere superusuario creado con: python manage.py createsuperuser
     path('admin/', admin.site.urls),
-    
-    # APLICACIÓN PRINCIPAL: registro_visitas
-    # URL base: http://dominio.com/
-    # Incluye todas las URLs definidas en registro_visitas/urls.py
-    # Esto permite que las rutas de la app sean accesibles desde la raíz
     path('', include('registro_visitas.urls')),
 ]
